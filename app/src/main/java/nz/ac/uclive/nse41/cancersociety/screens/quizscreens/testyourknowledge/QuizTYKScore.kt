51efa41b0b50b909bc5c7ae1ff3beaaf26e1bc4d
Nirhjah Selvarajah <nse41@uclive.ac.nz>
2024-10-17 09:38:34 +1300

    added image to score screen

--- app/src/main/java/nz/ac/uclive/nse41/cancersociety/screens/quizscreens/testyourknowledge/QuizTYKScore.kt
+++ app/src/main/java/nz/ac/uclive/nse41/cancersociety/screens/quizscreens/testyourknowledge/QuizTYKScore.kt
@@ -1,10 +1,14 @@
 package nz.ac.uclive.nse41.cancersociety.screens.quizscreens.testyourknowledge
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -14,10 +18,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import nz.ac.uclive.nse41.cancersociety.R
 import nz.ac.uclive.nse41.cancersociety.navigation.Screens
 import nz.ac.uclive.nse41.cancersociety.ui.theme.Bluey
 import nz.ac.uclive.nse41.cancersociety.ui.theme.CancerSocietyTheme
@@ -57,6 +63,27 @@ fun FinalScoreScreen(navController: NavController, finalScore: Int) {
                         }
                     ) {
                         Text("Main Menu")
+                    }
+
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.women1),
+                            contentDescription = "Woman 1",
+                            modifier = Modifier.size(250.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.women4),
+                            contentDescription = "Woman 2",
+                            modifier = Modifier.size(250.dp)
+                        )
+                        Image(
+                            painter = painterResource(id = R.drawable.men1),
+                            contentDescription = "Woman 3",
+                            modifier = Modifier.size(250.dp)
+                        )
                     }
 
 
